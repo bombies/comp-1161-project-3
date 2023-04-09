280969fe0cef099027d4f34ad1c69ae78b7d04f5
bombies <xjamesdzn@gmail.com>
2023-04-09 14:17:21 -0500

    Add RecommendationUtils

--- src/main/java/group/anmv/api/GPTService.java
+++ src/main/java/group/anmv/api/GPTService.java
@@ -2,10 +2,13 @@ package group.anmv.api;
 
 import com.theokanning.openai.OpenAiApi;
 import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.service.OpenAiService;
 import group.anmv.utils.config.ConfigHandler;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.theokanning.openai.service.OpenAiService.*;
@@ -20,21 +23,25 @@ public class GPTService {
             throw new NullPointerException("There is no API key for GPT-3");
 
         final var retrofit = defaultRetrofit(
-                defaultClient(key, Duration.of(10, TimeUnit.SECONDS.toChronoUnit())),
+                defaultClient(key, Duration.of(30, TimeUnit.SECONDS.toChronoUnit())),
                 defaultObjectMapper()
         );
 
         service = new OpenAiService(retrofit.create(OpenAiApi.class));
     }
 
-    public String getResponse(String prompt) {
-        final var req = CompletionRequest.builder()
-                .prompt(prompt)
-                .model("ada")
-                .echo(true)
+    public String getResponse(String prompt, String assistant) {
+        final var req = ChatCompletionRequest.builder()
+                .messages(
+                        List.of(
+                                new ChatMessage("user", prompt),
+                                new ChatMessage("assistant", assistant)
+                        )
+                )
+                .model("gpt-3.5-turbo")
                 .build();
-        final var choices = service.createCompletion(req).getChoices();
-        return !choices.isEmpty() ? choices.get(0).getText() : null;
+        final var choices = service.createChatCompletion(req).getChoices();
+        return !choices.isEmpty() ? choices.get(0).getMessage().getContent() : null;
     }
 
     public static GPTService instance() {
