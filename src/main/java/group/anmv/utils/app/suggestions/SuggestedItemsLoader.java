10ad9648a1e230eaacf8d15d064b08b1feb04f61
bombies <xjamesdzn@gmail.com>
2023-04-16 11:46:05 -0500

    Add asynchronous logic to loading suggestions

---
+++ src/main/java/group/anmv/utils/app/suggestions/SuggestedItemsLoader.java
@@ -0,0 +1,60 @@
+package group.anmv.utils.app.suggestions;
+
+import group.anmv.api.GPTService;
+import group.anmv.ui.components.ErrorFrame;
+import group.anmv.ui.components.LoadingFrame;
+import group.anmv.ui.components.SuggestionsFrame;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class SuggestedItemsLoader extends SwingWorker<List<String>, Object> {
+    private final List<String> items;
+    private SuggestionsFrame frame = new SuggestionsFrame();
+
+    public SuggestedItemsLoader(List<String> items) {
+        super();
+        this.items = items;
+    }
+
+    @Override
+    protected List<String> doInBackground() throws Exception {
+        frame.setLoading();
+        final var prompt = """
+                Given the following list of items:\s
+                """
+                + items.toString() +
+                """ 
+                         suggest some more similar items I could add to this shopping list.
+                                        
+                        Don't respond with additional courtesies or anything just the items in a comma separated fashion.
+                        """;
+        final var assistant = "The list should be formatted in such a way that all the elements are encapsulated in square brackets and comma separated";
+        final var asyncResponse = GPTService.instance().getResponse(prompt, assistant);
+        return asyncResponse.thenApply(response -> {
+                    final var suggestions = response
+                            .replaceAll("[\\[\\]]", "")
+                            .split(",\\s*");
+
+                    return Arrays.stream(suggestions)
+                            .filter(suggestion -> !items.contains(suggestion))
+                            .toList();
+                })
+                .whenComplete((responses, err) -> {
+                    if (err != null)
+                        new ErrorFrame(err.getMessage());
+                }).get();
+    }
+
+    @Override
+    protected void done() {
+        try {
+            frame.setItems(get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            new ErrorFrame(e.getMessage());
+        }
+    }
+}
